879fa9dae5b54f072b2636857278fc57e258a5e9
Jan Philip Göpfert <jgoepfert@techfak.uni-bielefeld.de>
2022-02-01 16:20:01 +0100

    Provide Image Segmentation

--- src/classicdata/__init__.py
+++ src/classicdata/__init__.py
@@ -1,6 +1,6 @@
 """
 Loaders for classic datasets.
 """
-from .datasets import Ionosphere, MagicGammaTelescope
+from .datasets import ImageSegmentation, Ionosphere, MagicGammaTelescope
 
-__all__ = ["Ionosphere", "MagicGammaTelescope"]
+__all__ = ["ImageSegmentation", "Ionosphere", "MagicGammaTelescope"]
